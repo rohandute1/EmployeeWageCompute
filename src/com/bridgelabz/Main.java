7febeb8ca7d522759b65151309c18fc06ccbbb34
Rohan <rohandute1@gmail.com>
2023-10-20 11:22:42 +0530

    [rohan]added:- successfully calculated daily wages of employee

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation!");
         final int IS_PRESENT = 1;
+        int wagePerHr = 20;
+        int fullDayHrs = 8;
+        int empWage;
         int random = (int)(Math.random()*10)%2;
         //System.out.println(random);
-        if (random == IS_PRESENT){
-            System.out.println("Employee is present.");
+        if (IS_PRESENT == random){
+            empWage = fullDayHrs*wagePerHr;
+            System.out.println("Employee is present and Employee Wage per day : "+ empWage);
         }
         else {
             System.out.println("Employee is absent.");
