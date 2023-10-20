866f949557a454a9e259a17a9fe5c6ba6e840704
Rohan <rohandute1@gmail.com>
2023-10-20 11:38:03 +0530

    [rohan] added:- successfullay checked employee is part time or not

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -6,12 +6,19 @@ public class Main {
         final int IS_PRESENT = 1;
         int wagePerHr = 20;
         int fullDayHrs = 8;
+        final int PART_TIME= 2;
+        int partTimeHrs = 4;
+        int partTimeEmpWage;
         int empWage;
-        int random = (int)(Math.random()*10)%2;
-        //System.out.println(random);
+        int random = (int)(Math.random()*10)%3;
+        System.out.println(random);
         if (IS_PRESENT == random){
             empWage = fullDayHrs*wagePerHr;
             System.out.println("Employee is present and Employee Wage per day : "+ empWage);
+        }
+        else if(PART_TIME==random){
+            partTimeEmpWage = wagePerHr * partTimeHrs;
+            System.out.println("Employee is part time and Employee Wage per day : "+ partTimeEmpWage);
         }
         else {
             System.out.println("Employee is absent.");
