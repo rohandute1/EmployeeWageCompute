aa3790a3d174464d64e2e154f2ba0c9de5599020
Rohan <rohandute1@gmail.com>
2023-11-15 13:43:08 +0530

    added:- implemented functionality so that we can save the total wage of each company

--- src/com/bridgelabz/EmployeeWageCompute.java
+++ src/com/bridgelabz/EmployeeWageCompute.java
@@ -3,12 +3,23 @@ package com.bridgelabz;
 public class EmployeeWageCompute {
     public static final int IS_PRESENT = 1;
     public static final int IS_PART_TIME= 2;
-    public static void calculateEmpWage(String company, int emoRatePerHour, int numOfWorkingDays, int  maxHoursPerMonth){
+    private final String company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+    int totalEmpWage;
+    public EmployeeWageCompute(String company, int emoRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
+        this.company = company;
+        this.empRatePerHour = emoRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+    }
+    public void calculateEmpWage(){
         int empHrs;
         int totalWorkingDays = 0;
         int totalEmpHrs = 0;
 
-        while(totalEmpHrs <=maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+        while(totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
         {
             totalWorkingDays++;
             int empCheck = (int)Math.floor(Math.random()*10)%3;
@@ -26,14 +37,21 @@ public class EmployeeWageCompute {
             }
             totalEmpHrs += empHrs;
         }
-        int totalEmpWage = totalEmpHrs * emoRatePerHour;
-        System.out.println("Total employee wage for company: " + company + "is: " + totalEmpWage);
+        totalEmpWage = totalEmpHrs * empRatePerHour;
+    }
+    @Override
+    public String toString(){
+        return "Total employee wage for company: " + company + " is: " + totalEmpWage;
     }
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation!");
-        calculateEmpWage("Bridgelabz", 22, 24, 100);
-        calculateEmpWage("Dmart", 25, 22, 80);
+        EmployeeWageCompute bridgeLabz = new EmployeeWageCompute("Bridgelabz",25,23, 120);
+        EmployeeWageCompute dMart = new EmployeeWageCompute("DMart",20,20, 105);
+        dMart.calculateEmpWage();
+        System.out.println(dMart);
+        bridgeLabz.calculateEmpWage();
+        System.out.println(bridgeLabz);
     }
 }
 
