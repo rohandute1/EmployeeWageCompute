7eda653735e809c5bab3cb657769119819e8fd61
Rohan <rohandute1@gmail.com>
2023-11-16 11:15:36 +0530

    added:- implemented interface to manage emp wage for multiple companies

--- src/com/bridgelabz/EmployeeWageCompute.java
+++ src/com/bridgelabz/EmployeeWageCompute.java
@@ -3,7 +3,11 @@ package com.bridgelabz;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmployeeWageCompute {
+interface EmpWageBuilder {
+    void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+    void calculateEmpWage();
+}
+public class EmployeeWageCompute implements EmpWageBuilder {
     public static final int IS_PRESENT = 1;
     public static final int IS_PART_TIME = 2;
 
@@ -13,11 +17,13 @@ public class EmployeeWageCompute {
         this.companyEmpWageList = new ArrayList<>();
     }
 
+    @Override
     public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
         CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
         companyEmpWageList.add(companyEmpWage);
     }
 
+    @Override
     public void calculateEmpWage() {
         for (CompanyEmpWage companyEmpWage : companyEmpWageList) {
             int empHrs;
@@ -26,7 +32,7 @@ public class EmployeeWageCompute {
 
             while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
                 totalWorkingDays++;
-                int empCheck = (int) (Math.random() * 10) % 3;
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
                 switch (empCheck) {
                     case IS_PRESENT:
                         empHrs = 8;
@@ -48,10 +54,10 @@ public class EmployeeWageCompute {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation!");
-        EmployeeWageCompute empWageCompute = new EmployeeWageCompute();
-        empWageCompute.addCompanyEmpWage("Bridgelabz", 25, 23, 120);
-        empWageCompute.addCompanyEmpWage("DMart", 20, 20, 105);
-        empWageCompute.calculateEmpWage();
+        EmpWageBuilder empWageBuilder = new EmployeeWageCompute();
+        empWageBuilder.addCompanyEmpWage("Bridgelabz", 25, 23, 120);
+        empWageBuilder.addCompanyEmpWage("DMart", 20, 20, 105);
+        empWageBuilder.calculateEmpWage();
     }
 }
 
